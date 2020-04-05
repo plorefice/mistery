b8dbf857d4c80374902361a08572f0c78868df4f
Pietro Lorefice <pietro.lorefice@gmail.com>
2020-04-05 14:55:44 +0200

    [WIP]

--- src/systems/input.rs
+++ src/systems/input.rs
@@ -25,14 +25,15 @@ impl fmt::Display for AxisBindings {
 /// Custom implementation for action bindings.
 #[derive(Clone, Debug, Hash, PartialEq, Eq, Serialize, Deserialize)]
 pub enum ActionBinding {
-    N,
-    W,
-    S,
-    E,
-    NW,
-    SW,
-    SE,
-    NE,
+    MoveN,
+    MoveW,
+    MoveS,
+    MoveE,
+    MoveNW,
+    MoveSW,
+    MoveSE,
+    MoveNE,
+    PickUp,
 }
 
 impl fmt::Display for ActionBinding {
@@ -57,12 +58,18 @@ impl BindingTypes for GameBindings {
 /// The system is used to dispatch user input to the player entity.
 #[derive(Default, SystemDesc)]
 pub struct InputDispatcher {
-    pressed: HashSet<ActionBinding>,
+    previous: HashSet<ActionBinding>,
+    current: HashSet<ActionBinding>,
 }
 
 impl InputDispatcher {
-    fn was_pressed(&self, action: &ActionBinding) -> bool {
-        self.pressed.contains(action)
+    fn update_actions(&mut self, actions: HashSet<ActionBinding>) {
+        std::mem::swap(&mut self.previous, &mut self.current);
+        self.current = actions;
+    }
+
+    fn active_actions(&self) -> impl Iterator<Item = &ActionBinding> {
+        self.current.difference(&self.previous)
     }
 }
 
@@ -83,47 +90,57 @@ impl<'s> System<'s> for InputDispatcher {
     ) {
         // Keep track of the actions which are down at each update
         // to implement non-repeating key press events.
-        let pressed: HashSet<_> = input
-            .bindings
-            .actions()
-            .cloned()
-            .filter(|a| input.action_is_down(a).unwrap_or_default())
-            .collect();
+        self.update_actions(
+            input
+                .bindings
+                .actions()
+                .filter(|a| input.action_is_down(a).unwrap_or_default())
+                .cloned()
+                .collect(),
+        );
 
+        // Do nothing if no action is active
+        if self.active_actions().count() == 0 {
+            return;
+        }
+
+        // This should actually loop only once right now
         for (e, _, &Position(p), actor) in (&entities, &listeners, &positions, &mut actors).join() {
+            if !actor.perform() {
+                continue;
+            }
+
             let mut to = p;
 
-            for action in pressed.iter().filter(|a| !self.was_pressed(a)) {
+            for action in self.active_actions() {
                 match action {
-                    ActionBinding::N => to[1] += 1,
-                    ActionBinding::W => to[0] -= 1,
-                    ActionBinding::S => to[1] -= 1,
-                    ActionBinding::E => to[0] += 1,
-                    ActionBinding::NW => {
+                    ActionBinding::MoveN => to[1] += 1,
+                    ActionBinding::MoveW => to[0] -= 1,
+                    ActionBinding::MoveS => to[1] -= 1,
+                    ActionBinding::MoveE => to[0] += 1,
+                    ActionBinding::MoveNW => {
                         to[1] += 1;
                         to[0] -= 1;
                     }
-                    ActionBinding::SW => {
+                    ActionBinding::MoveSW => {
                         to[1] -= 1;
                         to[0] -= 1;
                     }
-                    ActionBinding::SE => {
+                    ActionBinding::MoveSE => {
                         to[1] -= 1;
                         to[0] += 1;
                     }
-                    ActionBinding::NE => {
+                    ActionBinding::MoveNE => {
                         to[1] += 1;
                         to[0] += 1;
                     }
+                    ActionBinding::PickUp => {}
                 }
             }
 
-            if to != p && actor.perform() {
+            if to != p {
                 movers.insert(e, WantsToMove { to }).unwrap();
             }
         }
-
-        // Store currently active actions
-        self.pressed = pressed;
     }
 }
